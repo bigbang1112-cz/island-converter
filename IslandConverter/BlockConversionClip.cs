870ce49441d3a0796b7bc48bbdf132e103d08ea8
Petr Pivoňka <petrpiv1@gmail.com>
2021-07-07 03:01:46 +0200

    Init commit

---
+++ IslandConverter/BlockConversionClip.cs
@@ -0,0 +1,11 @@
+﻿namespace IslandConverter
+{
+    public class BlockConversionClip
+    {
+        public string ItemModel { get; set; }
+        public int[] OffsetCoord { get; set; }
+        public float[] OffsetPosition { get; set; }
+        public int OffsetDirection { get; set; }
+        public float[] CenterFromCoord { get; set; }
+    }
+}
